de4ec625bd4065d5753e649e32d0216626fb8119
Lucas Spigariol <lspigariol@gmail.com>
2019-11-15 10:30:07 -0300

    solucion

--- fabrica.hs
+++ fabrica.hs
@@ -1,16 +1,128 @@
-
 data Producto = UnProducto {
-	nombre :: String,
-	precio :: Int,
-	componentes :: [(String, Int)]
-	} deriving Show
-	
-data Pedido = UnPedido {
-	cliente :: String,
-	nombreProducto:: String,
-	cantidad :: Int} deriving Show
+  nombre :: String,
+  precio :: Int,
+  componentes :: [(Componente, Int)]
+} deriving Show
 
-data PersonaACargo :: Trabajador {
-	descripcion:: String
---  ... :: ...
-}	
+data Pedido = UnPedido {
+  cliente :: String,
+  nombreProducto:: String,
+  cantidad :: Int
+} deriving Show
+
+data PersonaACargo = Trabajador {
+  descripcion:: String,
+  especialidad:: Especialidad
+}
+
+type Especialidad = [Producto] -> Pedido -> Bool
+type Componente = String
+
+---------------------------------------------------------------------------------
+-- 1) Lo primero que se quiere saber es:
+
+--los productos pedidos sin precio
+productosPedidosSinPrecio :: [Producto] -> [Pedido]-> [String]
+productosPedidosSinPrecio productos pedidos =
+   map nombreProducto (filter (not.existeEn productos) pedidos )
+
+existeEn:: [Producto] ->Pedido -> Bool
+existeEn productos pedido = any (coincide pedido) productos
+
+coincide :: Pedido -> Producto -> Bool
+coincide pedido producto = nombreProducto pedido == nombre producto
+
+--los productos baratos
+productosBaratos:: Int -> [Producto] -> [String]
+productosBaratos importe productos = (map nombre. filter (esBarato importe)) productos
+
+esBarato:: Int -> Producto -> Bool
+esBarato importe producto = precio producto < importe
+
+--los componentes que no son exclusivos
+componentesNoExclusivos::[Producto] -> [Componente] -> [Componente] 
+componentesNoExclusivos productos componentes 
+  = filter (componenteNoExclusivo productos) componentes 
+
+componenteNoExclusivo::[Producto] -> Componente -> Bool 
+componenteNoExclusivo productos componente 
+  = length (filter  (usa componente)  productos) > 1 
+  
+usa:: Componente -> Producto -> Bool
+usa componente producto = any ((componente ==).fst) (componentes producto)
+
+----------------------------------------------------------------------------------
+-- 2) Además, se requiere ver la factibilidad de cumplir con los pedidos recibidos
+-- A partir de todos los pedidos recibidos 
+-- y de saber quién está a cargo de la fabrica en ese momento, 
+-- encontrar aquellos que se puedan cumplir.
+
+sePuedenCumplir::[Pedido] -> PersonaACargo -> [Producto] -> [Pedido]
+sePuedenCumplir pedidos persona productos = filter ((especialidad persona) productos) pedidos
+
+
+-- Especialidades
+vendedor:: Int -> Especialidad
+vendedor importe productos pedido = precio (productoDelPedido productos pedido) < importe
+
+gerente:: Especialidad
+gerente _ _ = True
+
+ingeniero:: Especialidad
+ingeniero productos pedido = cantidadComponentes (productoDelPedido productos pedido) * cantidad pedido < 100
+
+cantidadComponentes :: Producto -> Int
+cantidadComponentes producto = sum (map snd (componentes producto))
+
+productoDelPedido:: [Producto] -> Pedido -> Producto
+productoDelPedido productos pedido = head (filter (coincide pedido) productos)
+
+-----------------------------------------------------------------------
+-- EJEMPLOS DE PRUEBA
+
+auto = UnProducto "auto" 1000 [("rueda",4),("motor",1)]
+avion = UnProducto "avion" 5000 [("ala",2),("motor",4)]
+
+pedidoAuto = UnPedido "acme" "auto" 100
+pedidoAvion = UnPedido "acme" "avion" 1
+pedidoBarco = UnPedido "pdep" "barco" 1
+pedidoMasAutos = UnPedido "pdep" "auto" 3
+
+pedidosEjemplo = [pedidoAuto, pedidoAvion,pedidoBarco,pedidoMasAutos]
+pedidosEjemplo2 = [pedidoAuto, pedidoAvion,pedidoMasAutos]
+componentesEjemplo = ["rueda","motor","ala"]
+productosEjemplo = [auto,avion]
+
+
+cacho = Trabajador "saraza" (vendedor 2000)
+pepo = Trabajador "saraza" (vendedor 2000000)
+tito = Trabajador "saraza" gerente
+toni = Trabajador "saraza" ingeniero
+
+---------------------------------------------------
+-- CONSULTAS
+{-
+*Main> productosPedidosSinPrecio productosEjemplo pedidosEjemplo 
+["barco"]
+
+*Main> productosBaratos 2000 productosEjemplo 
+["auto"]
+*Main> productosBaratos 200000 productosEjemplo 
+["auto","avion"]
+
+*Main> componentesNoExclusivos productosEjemplo componentesEjemplo 
+["motor"]
+
+*Main> sePuedenCumplir pedidosEjemplo2 cacho productosEjemplo 
+[UnPedido {cliente = "acme", nombreProducto = "auto", cantidad = 100},UnPedido {cliente = 
+"pdep", nombreProducto = "auto", cantidad = 3}]
+*Main> sePuedenCumplir pedidosEjemplo2 pepo productosEjemplo 
+[UnPedido {cliente = "acme", nombreProducto = "auto", cantidad = 100},UnPedido {cliente = 
+"acme", nombreProducto = "avion", cantidad = 1},UnPedido {cliente = "pdep", nombreProducto = "auto", cantidad = 3}]
+*Main> sePuedenCumplir pedidosEjemplo2 tito productosEjemplo    
+[UnPedido {cliente = "acme", nombreProducto = "auto", cantidad = 100},UnPedido {cliente = 
+"acme", nombreProducto = "avion", cantidad = 1},UnPedido {cliente = "pdep", nombreProducto = "auto", cantidad = 3}]
+*Main> sePuedenCumplir pedidosEjemplo2 toni productosEjemplo 
+[UnPedido {cliente = "acme", nombreProducto = "avion", cantidad = 1},UnPedido {cliente = "pdep", nombreProducto = "auto", cantidad = 3}]
+
+-}
--- fabrica.pl
+++ fabrica.pl
@@ -1,21 +1,34 @@
+
 %requiere(producto, componente,cantidad).
 requiere(auto, rueda, 4).
 requiere(auto, motor, 1).
-requiere(moto, rueda, 2) 
+requiere(moto, rueda, 2). 
 requiere(avion, motor, 4).
 requiere(avion, ala, 2).
+
 %precio(producto,importe).
 precio(auto, 1000).
 precio(camion, 2000).
 precio(moto, 500).
 precio(avion, 5000).
+
 %pedido(cliente,producto, cantidad).
 pedido(acme, auto, 10).
 pedido(acme, avion, 1).
 pedido(pdepTransportes, barco, 1).
 pedido(pdepTransportes, auto, 3).
+
 %stock(componente, stock).
-stock(rueda,20),
-stock(motor,10),
+stock(rueda,20).
+stock(motor,10).
 stock(ala,3).
 
+
+productoPedidoSinPrecio(Producto):-
+   pedido(_, Producto,_),
+   not( precio(Producto,_)).
+   
+
+productoBarato(Tope,Producto):-
+    precio(Producto,Precio),
+    Precio < Tope.
